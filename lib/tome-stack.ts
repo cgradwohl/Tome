0485921b09cec62cef3b136b99fd86162989199d
Chris Gradwohl <christophergradwohl@gmail.com>
2020-06-17 15:32:39 -0700

    notes.

--- lib/tome-stack.ts
+++ lib/tome-stack.ts
@@ -5,5 +5,18 @@ export class TomeStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // Auth:
+    // Cognito User Pool
+    // Lambda
+    // DynamoDB Table
+
+    // User Data: Profile, Library, Group Meta, Queue, Tome
+    // API Gateway
+    // Cognito Authorizer
+    // Lambda w/ read access to DDD
+    // DynamoDB Table
+
+    // TODO: folder structure (infra vs application code .... or are they the same?   :))
   }
 }
