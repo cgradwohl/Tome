6c0ee16bbe82a50532427e0c01fabe87d152282c
Chris Gradwohl <christophergradwohl@gmail.com>
2020-06-29 22:18:03 -0700

    jes.

--- lambdas/postAuthTrigger.ts
+++ lambdas/postAuthTrigger.ts
@@ -1,3 +1,8 @@
-export const handler = async (event: any = {}) : Promise <any> => {
+export const handler = async (event: any = {}): Promise<any> => {
+  console.log ("Authentication successful");
+  console.log ("Trigger function =", event.triggerSource);
+  console.log ("User pool = ", event.userPoolId);
+  console.log ("App client ID = ", event.callerContext.clientId);
+  console.log ("User ID = ", event.userName);
   return { statusCode: 201, body: ' [ POST AUTH TRIGGER ] ' + event };
 };
--- lambdas/preAuthTrigger.ts
+++ lambdas/preAuthTrigger.ts
@@ -1,3 +1,4 @@
-export const handler = async (event: any = {}) : Promise <any> => {
+export const handler = async (event: any = {}): Promise<any> => {
+  // Return to Amazon Cognito
   return { statusCode: 201, body: ' [ PRE AUTH TRIGGER ] ' + event};
 };
